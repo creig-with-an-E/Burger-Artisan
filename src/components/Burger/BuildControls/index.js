fe3155c7072c0b9511fc98f5d013d34b39bc8fe2
creig-with-an-E <creig.phiri@gmail.com>
2024-01-28 21:19:54 -0700

    remove signup link

--- src/components/Burger/BuildControls/index.js
+++ src/components/Burger/BuildControls/index.js
@@ -28,8 +28,6 @@ const buildControls =props =>{
               />
           )})
       }
-      <button className={classes.OrderButton} disabled={!props.purchasable} 
-        onClick={props.purchaseHandler}>{props.isAuthenticated ? "ORDER NOW" : "SIGNUP TO CONTINUE"}</button>
     </div>
   )
 }
